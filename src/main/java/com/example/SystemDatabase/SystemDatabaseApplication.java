c9e18fb909b67cfb2a730c70c470bf83d4a0437b
Walid Hanosh <walid.hanosh@gmail.com>
2018-12-25 17:14:06 +0200

    First Initial Commit

---
+++ src/main/java/com/example/SystemDatabase/SystemDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.SystemDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDatabaseApplication.class, args);
+	}
+
+}
+
